24c607b54c1578fdfc110f0b0f57549d39cb13cd
Leslie-Fang <fangjiahao@sjtu.edu.cn>
2016-10-17 22:07:53 +0800

    add

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,19 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Question
+from django.template import loader
 
 def hello_poll(request):
     return HttpResponse("Polls View!")
 
 def second_poll(request):
     return HttpResponse("second Polls View!")
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
